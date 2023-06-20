04f27ecc04ee360d776ffc0616ec0bda1d173e5c
Ana-Maria-Iulia Cornea <iulia.cornea@tum.de>
2023-06-20 20:32:09 +0200

    enable Traefik on k3s cluster

--- terraform/modules/cluster/main.tf
+++ terraform/modules/cluster/main.tf
@@ -26,7 +26,6 @@ locals {
   token = "${random_password.bootstrap_token_id.result}.${random_password.bootstrap_token_secret.result}"
   common_k3s_args = [
     "--kube-apiserver-arg", "enable-bootstrap-token-auth",
-    "--disable", "traefik",
     "--node-label", "az=${var.availability_zone}",
   ]
 }
